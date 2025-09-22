0f5b15d4e13a828232ba80015af7f5d80dfd6b33
Gautier <hybanos@orange.fr>
2025-09-22 14:36:22 +0200

    fix f3 crosshair

--- src/main/java/hybanos/addon/mixin/InGameHudMixin.java
+++ src/main/java/hybanos/addon/mixin/InGameHudMixin.java
@@ -4,6 +4,7 @@ import meteordevelopment.meteorclient.systems.modules.Modules;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.client.option.Perspective;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.client.render.Camera;
 import net.minecraft.client.option.GameOptions;
@@ -17,40 +18,17 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(InGameHud.class)
 public class InGameHudMixin {
-    /*
 
-    @Inject(method="renderCrosshair", at=@At(value = "HEAD"), cancellable = true)
-    private void onRenderCrosshair(DrawContext context, CallbackInfo info) {
-
+    @Inject(method = "shouldRenderCrosshair", at=@At(value= "HEAD"), cancellable = true)
+    private void onShouldRenderCrosshair(CallbackInfoReturnable<Boolean> cir) {
         if (Modules.get().isActive(F3_crosshair.class)) {
-            MinecraftClient mc = MinecraftClient.getInstance();
-            GameOptions gameOptions = mc.options;
-
-            if (!gameOptions.getPerspective().isFirstPerson() && !Modules.get().get(F3_crosshair.class).getPerspective()) {
-                info.cancel();
-                return;
-            }
-
-            if (mc.interactionManager.getCurrentGameMode() != GameMode.SPECTATOR) {
-                Camera camera = mc.gameRenderer.getCamera();
-                Matrix4fStack matrixStack = RenderSystem.getModelViewStack();
-                matrixStack.push();
-                matrixStack.multiplyPositionMatrix(context.getMatrices().peek().getPositionMatrix());
-                matrixStack.translate(mc.getWindow().getScaledWidth() / 2, mc.getWindow().getScaledHeight() / 2, 0.0f);
-                matrixStack.multiply(RotationAxis.NEGATIVE_X.rotationDegrees(camera.getPitch()));
-                matrixStack.multiply(RotationAxis.POSITIVE_Y.rotationDegrees(camera.getYaw()));
-                matrixStack.scale(-1.0f, -1.0f, -1.0f);
-                RenderSystem.applyModelViewMatrix();
-                RenderSystem.renderCrosshair(10);
-                matrixStack.pop();
-                RenderSystem.applyModelViewMatrix();
-                info.cancel();
-            }
+            MinecraftClient client = MinecraftClient.getInstance();
+            boolean val = client.options.getPerspective() == Perspective.FIRST_PERSON && !client.player.hasReducedDebugInfo() && client.options.getReducedDebugInfo().getValue() == false;
+            cir.setReturnValue(val);
         }
     }
-
-     */
 }
--- src/main/java/hybanos/addon/modules/haha/F3_crosshair.java
+++ src/main/java/hybanos/addon/modules/haha/F3_crosshair.java
@@ -7,18 +7,7 @@ import meteordevelopment.meteorclient.systems.modules.Module;
 public class F3_crosshair extends Module {
     private final SettingGroup sgGeneral = settings.getDefaultGroup();
 
-    private final Setting<Boolean> perspective = sgGeneral.add(new BoolSetting.Builder()
-        .name("Third person")
-        .description("Displays the crosshair even in third person.")
-        .defaultValue(true)
-        .build()
-    );
-
     public F3_crosshair() {
         super(HAHAddon.CATEGORY, "F3 Crosshair", "Renders the f3 crosshair.");
-    }
-
-    public boolean getPerspective() {
-        return perspective.get();
     }
 }
